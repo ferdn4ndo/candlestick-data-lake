03de3c20804a61e41e3626250b633ad887a87aad
autoblack <ferdn4ndo@users.noreply.github.com>
2021-03-31 22:22:37 +0000

    fixup: Format Python code with Black

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,6 +3,11 @@ import os
 
 __version__ = "0.0.1"
 APP_PORT = os.getenv("APP_PORT", "8888")
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "0").lower() in ["true", "1", "y", "yes"]
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "0").lower() in [
+    "true",
+    "1",
+    "y",
+    "yes",
+]
 STATIC_PATH = os.path.join(os.path.dirname(__file__), "static")
 tornado_app = None
