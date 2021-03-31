39365cd5fea7ae8c506660547de87e003a9aaaa7
Fernando Constantino <const.fernando@gmail.com>
2021-03-31 19:22:00 -0300

    adjusted env variables check

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,9 +2,7 @@ import os
 
 
 __version__ = "0.0.1"
-APP_PORT = os.environ["APP_PORT"] if "APP_PORT" in os.environ else 8888
-DEVELOPMENT_MODE = (
-    os.environ["DEVELOPMENT_MODE"] == "1" if "DEVELOPMENT_MODE" in os.environ else False
-)
+APP_PORT = os.getenv("APP_PORT", "8888")
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "0").lower() in ["true", "1", "y", "yes"]
 STATIC_PATH = os.path.join(os.path.dirname(__file__), "static")
 tornado_app = None
