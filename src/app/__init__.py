75bef6877cab8c815ceccf9e6692342beba27feb
Fernando Constantino <const.fernando@gmail.com>
2021-03-31 00:23:18 -0300

    changed version

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-__version__ = "1.0.0"
+__version__ = "0.0.1"
 APP_PORT = os.environ['APP_PORT'] if 'APP_PORT' in os.environ else 8888
 DEVELOPMENT_MODE = os.environ['DEVELOPMENT_MODE'] == '1' if 'DEVELOPMENT_MODE' in os.environ else False
 tornado_app = None
